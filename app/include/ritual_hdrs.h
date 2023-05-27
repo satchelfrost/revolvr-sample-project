363458a3583444e26678ddf952c64b55a5fed0ec
Reese Gallagher <reese.gallagher@infinitasinc.com>
2023-05-27 13:36:13 -0400

    Reorganziation of internal app structure

---
+++ app/include/ritual_hdrs.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "spinning_pointer.h"
+#include "hand_behavior.h"
+#include "moving_sound_box.h"
+#include "spawn_box.h"
+#include "timer_box_maker.h"
+#include "conway_box_maker.h"
