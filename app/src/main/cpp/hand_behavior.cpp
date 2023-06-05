601e0b99c7502a9b7bb6e23a8a2a9e4461eae844
Reese Gallagher <reese.gallagher@infinitasinc.com>
2023-06-05 11:08:31 -0400

    Updated collision scene

--- app/src/main/cpp/hand_behavior.cpp
+++ app/src/main/cpp/hand_behavior.cpp
@@ -2,6 +2,7 @@
 #include "common.h"
 #include "ecs/ecs.h"
 #include "ecs/component/types/spatial.h"
+#include "ecs/component/types/mesh.h"
 #include "action/io.h"
 #include "global_context.h"
 #include "helper_macros.h"
@@ -23,19 +24,14 @@ void HandBehavior::Update(float delta) {
     float multiplier = 1.0f - 0.5f * value;
     glm::vec3 newScale = initScale_ * multiplier;
     spatial_->SetLocalScale(newScale);
+
+    if (ButtonPressed(rvr::ActionType::A)) {
+        auto mesh = GetComponent<rvr::Mesh>(id);
+        mesh->SetVisibilityRecursive(true);
+    }
 }
 
 void HandBehavior::OnTriggered(rvr::Collider* other) {
-    // Check for dummy collision i.e. hands are not tracked yet and so they will be at the origin
-    auto hand1 = GetComponent<rvr::Spatial>(id);
-    auto hand2 = GetComponent<rvr::Spatial>(other->id);
-    auto hand1Position = hand1->GetWorld().GetPosition();
-    auto hand2Position = hand2->GetWorld().GetPosition();
-
-    if (hand1Position == glm::zero<glm::vec3>() ||
-        hand2Position == glm::zero<glm::vec3>())
-        return;
-
-    Log::Write(Log::Level::Info, Fmt("Collision source %d", id));
-    Log::Write(Log::Level::Info, Fmt("Collision other %d", other->id));
+    auto mesh = GetComponent<rvr::Mesh>(other->id);
+    mesh->SetVisibilityRecursive(false);
 }
