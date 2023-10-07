7cb4063f7d181c8589d5edd6250eac0a57414331
Reese Gallagher <reese.gallagher@infinitasinc.com>
2023-10-06 20:01:12 -0400

    Updated to the Kepler equation for the example

--- app/include/orbiting_lights.h
+++ app/include/orbiting_lights.h
@@ -7,9 +7,13 @@ class OrbitingLights : public rvr::Ritual {
     std::vector<rvr::Spatial*> lightSpatials_;
     float period_ = 5.0f;
     float currentTime_ = 0.0f;
-    float a_ = 2.0f; // semi-major axis Kepler's First Law
-    float e_ = 0.7f; // eccentricity Kepler's First Law
+    float apoapsis_ = 2.2f;  //https://en.wikipedia.org/wiki/Apsis
+    float periapsis_ = 0.5f; // https://en.wikipedia.org/wiki/Apsis
 
+    // Mean anomaly (M) and eccentric anomaly (E) (https://en.wikipedia.org/wiki/Kepler%27s_equation)
+    static float KeplerEquation(float M , float E, float e) ;
+    static float KeplerDifferentiated(float E, float e) ;
+    static float SolveKepler(float M, float e);
 public:
     OrbitingLights(rvr::type::EntityId id);
     virtual void Update(float delta) override;
--- app/src/main/cpp/orbiting_lights.cpp
+++ app/src/main/cpp/orbiting_lights.cpp
@@ -5,7 +5,8 @@
 #define LIGHT_1_ID 7
 #define LIGHT_2_ID 8
 #define LIGHT_3_ID 9
-#define TWO_PI (2.0f * 3.14159f)
+#define PI (3.14159f)
+#define TWO_PI (2.0f * PI)
 
 OrbitingLights::OrbitingLights(rvr::type::EntityId id) : Ritual(id) {
     lightSpatials_.push_back(GetComponent<rvr::Spatial>(LIGHT_1_ID));
@@ -15,13 +16,43 @@ OrbitingLights::OrbitingLights(rvr::type::EntityId id) : Ritual(id) {
 
 void OrbitingLights::Update(float delta) {
     currentTime_ += delta;
-    float angularSpeed = TWO_PI / period_;
-    float angle = angularSpeed * currentTime_;
+
+    float semiMajorAxis = (apoapsis_ + periapsis_) / 2.0f;
+    float semiMinorAxis = sqrt(apoapsis_ * periapsis_);
+    float linearEccentricity = semiMajorAxis - periapsis_;
+    float eccentricity = linearEccentricity / semiMajorAxis;
+    float meanAnomaly = currentTime_ * TWO_PI / period_;
+
     for (int i = 0; i < lightSpatials_.size(); i++) {
         float angleOffset = (TWO_PI / (float)lightSpatials_.size()) * (float)i;
-        float radius = a_ * (1 - (e_ * e_)) / (1.0f + e_ * (float)cos(angle + angleOffset));
-        float x =  radius * (float)cos(angle + angleOffset);
-        float z = -radius * (float)sin(angle + angleOffset);
+        float eccentricAnomaly = SolveKepler(meanAnomaly + angleOffset, eccentricity);
+        float x =  semiMajorAxis * ((float)cos(eccentricAnomaly) - eccentricity);
+        float z = -semiMinorAxis * (float)sin(eccentricAnomaly);
         lightSpatials_[i]->SetLocalPosition({x, 0.0f, z});
     }
+}
+
+float OrbitingLights::KeplerEquation(float M, float E, float e) {
+    return E - (e * sin(E)) - M;
+}
+
+float OrbitingLights::KeplerDifferentiated(float E, float e) {
+    return 1 - (e * cos(E));
+}
+
+float OrbitingLights::SolveKepler(float M, float e) {
+    float accuracy = 0.000001f;
+    int iterations = 100;
+
+    // Pick a suitable starting value for Newton's Method
+    float E = (e > 0.8f) ? PI : M;
+    for (int i = 0; i < iterations; i++) {
+        float ENext = E - (KeplerEquation(M, E, e) / KeplerDifferentiated(E, e));
+        float diff = abs(E - ENext);
+        E = ENext;
+        if (diff < accuracy)
+            break;
+    }
+
+    return E;
 }
